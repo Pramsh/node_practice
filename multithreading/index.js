fe4d4f50ef9f0b16bb7f95f689a27ba50b47d2aa
Pramsh <rpramstrahler@gmail.com>
2024-09-12 00:16:53 +0200

    first case

--- multithreading/index.js
+++ multithreading/index.js
@@ -1,32 +1,29 @@
 const express = require("express")
-const app = express()
 
+const app = express()
 const PORT = 3000
 
-app.get("/fast-page", (req,res) => 
+app.get("/fast-page", (req,res) =>
     res.status(200).send("This page is fast")
 )
 
-
 const slowLogic = () => {
     let data = []
-const sample = {
-        a:"complexdata",
-        b:"complexdata",
-        c:"complexdata",
-        d:"complexdata"
+    const sample = {
+            a:"complexdata",
+            b:"complexdata",
+            c:"complexdata",
+            d:"complexdata"
+        }
+    for (let i = 0; i < 13_000_000; i++){
+        data.push({...sample, n:i})
     }
-
-for (let i = 0; i < 13_000_000; i++){
-    data.push({...sample, n:i})
-}
 }
 
 app.get("/slow-page", (req,res) => {
     slowLogic() 
    res.status(200).send("This page is SLOW")
 })
-
 
 app.listen(PORT,() => {
     console.log("app is listening on port "+PORT);
