0969673351f646d98c8d7aabf508cd21abfccdb7
Pramsh <rpramstrahler@gmail.com>
2024-09-13 22:58:09 +0200

    removed comments

--- cluster/index.js
+++ cluster/index.js
@@ -2,25 +2,25 @@ import cluster from "cluster"
 import express from "express"
 import { cpus } from 'os'
 const PORT = 3000
-// const allProcesses = {}
-// if(cluster.isPrimary){
-//     console.log("master process going on pin: "+process.pid);
-//     const cpuCount = cpus().length
-//     for (let i = 0; i < cpuCount; i++){
-//         const worker = cluster.fork()
+const allProcesses = {}
+if(cluster.isPrimary){
+    console.log("master process going on pin: "+process.pid);
+    const cpuCount = cpus().length
+    for (let i = 0; i < cpuCount; i++){
+        const worker = cluster.fork()
 
-//         worker.on("message", (message) => {
-//             if(message.type === "updateProcess") {
-//                 const { pid } = message
-//                 if(!(pid in allProcesses)){
-//                     allProcesses[pid] = 0
-//                 }else{
-//                     allProcesses[pid] += 1 
-//                 }
-//             }
-//         })
-//     }
-// }else{
+        worker.on("message", (message) => {
+            if(message.type === "updateProcess") {
+                const { pid } = message
+                if(!(pid in allProcesses)){
+                    allProcesses[pid] = 0
+                }else{
+                    allProcesses[pid] += 1 
+                }
+            }
+        })
+    }
+}else{
     const app = express()
     app.get("/", (req,res) => {          
         let a = 0
@@ -28,10 +28,10 @@ const PORT = 3000
             a += i
         }     
         
-        // process.send({
-        //     type: 'updateProcess',
-        //     pid: process.pid
-        // });
+        process.send({
+            type: 'updateProcess',
+            pid: process.pid
+        });
 
         res.status(200).send("totale is "+a)
     } )
@@ -40,4 +40,4 @@ const PORT = 3000
         console.table({PORT, pid:process.pid});
         
     })
-// }
+}
